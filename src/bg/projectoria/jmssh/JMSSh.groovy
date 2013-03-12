91e740fb44d8a5b401b192a623d3aa2e6638e734
Kaloian Doganov <doganov@projectoria.bg>
2013-03-13 00:40:47 +0200

    Small useless embedded Groovy Shell.

--- src/bg/projectoria/jmssh/JMSSh.groovy
+++ src/bg/projectoria/jmssh/JMSSh.groovy
@@ -1,7 +1,15 @@
 package bg.projectoria.jmssh
 
 import org.codehaus.groovy.tools.shell.Groovysh
+import org.codehaus.groovy.tools.shell.IO
 
 class JMSSh extends Groovysh {
 
+    JMSSh(IO io) {
+        super(io);
+    }
+
+    JMSSh() {
+        super();
+    }
 }
